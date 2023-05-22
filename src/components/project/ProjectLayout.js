1ad117f64d39c88e946e8c46323bc3f61816a366
Revilise <nice.mutnykh@mail.ru>
2023-05-22 20:46:39 +0300

    .

--- src/components/project/ProjectLayout.js
+++ src/components/project/ProjectLayout.js
@@ -5,9 +5,14 @@ import {useRouter} from "next/router";
 import {useState} from "react";
 import Layout from "../layout/Layout";
 import Link from "next/link";
+import Button from "../button/Button";
 
 export default function ProjectLayout({children, projectId}) {
     const [url] = useState(`/projects/${projectId}`)
+
+    function LeaveProject() {
+
+    }
 
     return (
         <Layout.Semantic>
@@ -16,6 +21,7 @@ export default function ProjectLayout({children, projectId}) {
                     <Link href={`${url}/step-1`}>Этап 1</Link>
                     <Link href={`${url}/members`}>Участники</Link>
                     <Link href={`${url}/file-store`}>Хранилище файлов</Link>
+                    <Button>Покинуть проект</Button>
                 </Header>
                 {children}
             </div>
